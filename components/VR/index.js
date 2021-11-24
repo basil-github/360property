438db41500289ce15fd9c5abbdb92c4fe7721ac3
Basil <basil@centresource.in>
2021-11-24 17:42:30 +0530

    vr set don withb dummy json

--- components/VR/index.js
+++ components/VR/index.js
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Pannellum } from "@georgedrpg/pannellum-react-next";
-let myImage = [
-  {
-    index: 1,
-    link: "https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/tunnel.jpg",
-  },
-  {
-    index: 2,
-    link: "https://miro.medium.com/max/10752/1*kY8Q5mqqqMz6r8YxcEcU6w.jpeg",
-  },
-];
+import properties from "../../public/Data/properties.json";
+
 function VR() {
-  const [state, setstate] = useState(myImage[0]);
-  console.log(state.index);
+  const [state, setstate] = useState(properties[0].images[0]);
+  //console.log(state);
+
   return (
     <div>
       <Pannellum
@@ -27,15 +20,52 @@ function VR() {
           console.log("panorama loaded");
         }}
       >
-        <Pannellum.Hotspot
-          type="custom"
-          pitch={31}
-          yaw={150}
-          handleClick={(evt, name) => setstate(myImage[1])}
-          name="hs1"
-          text="sdf"
-        />
+        {state?.hotspot?.map((spot, i) => (
+          <Pannellum.Hotspot
+            key={i}
+            type="custom"
+            pitch={spot.pitch}
+            yaw={spot.yaw}
+            handleClick={(evt, name) =>
+              setstate(properties[0].images[spot.index])
+            }
+            name="hs1"
+            text="sdf"
+          />
+        ))}
       </Pannellum>
+      {/* {state.index == 1 && components && (
+        <Pannellum
+          width="100%"
+          height="100vh"
+          image={state.link}
+          pitch={10}
+          yaw={180}
+          hfov={110}
+          autoLoad
+          onLoad={() => {
+            console.log("panorama loaded");
+          }}
+          hotspotDebug
+        >
+          <Pannellum.Hotspot
+            type="custom"
+            pitch={posi}
+            yaw={500}
+            handleClick={(evt, name) => setstate(myImage[1])}
+            name="hs1"
+            text="sdf"
+          />
+          <Pannellum.Hotspot
+            type="custom"
+            pitch={20.596368557706256}
+            yaw={-84.6974409999092}
+            handleClick={(evt, name) => setstate(myImage[1])}
+            name="hs1"
+            text="sdf"
+          />
+        </Pannellum>
+      )} */}
     </div>
   );
 }
