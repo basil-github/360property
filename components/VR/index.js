8a9c428182d7a8c7a48d4857a133df940386f564
Basil <basil@centresource.in>
2021-11-28 16:24:44 +0530

    build

--- components/VR/index.js
+++ components/VR/index.js
@@ -2,16 +2,25 @@ import React, { useEffect, useState } from "react";
 import { Pannellum } from "@georgedrpg/pannellum-react-next";
 import properties from "../../public/Data/properties.json";
 
-function VR() {
-  const [state, setstate] = useState(properties[0].images[0]);
-  //console.log(state);
+function VR({ properties }) {
+  let fistVRid = properties[0].Images[0].vr_image.id;
+
+  const findImage = (id) => {
+    const filter = properties[0].Images.filter((img) => {
+      if (img.vr_image.id == id) {
+        return true;
+      }
+    });
+    return filter;
+  };
+  const [state, setstate] = useState(findImage(fistVRid));
 
   return (
-    <div>
+    <div className="main_vr">
       <Pannellum
         width="100%"
         height="100vh"
-        image={state.link}
+        image={process.env.API_BASE_URL + state[0]?.vr_image?.Image?.url}
         pitch={10}
         yaw={180}
         hfov={1100}
@@ -21,20 +30,30 @@ function VR() {
           console.log("panorama loaded");
         }}
       >
-        {state?.hotspot?.map((spot, i) => (
+        {state[0]?.Hotspot?.map((spot, i) => (
           <Pannellum.Hotspot
             key={i}
             type="custom"
             pitch={spot.pitch}
             yaw={spot.yaw}
-            handleClick={(evt, name) =>
-              setstate(properties[0].images[spot.index])
-            }
+            handleClick={(evt, name) => setstate(findImage(spot.vr_image.id))}
             name="hs1"
             text="sdf"
           />
         ))}
       </Pannellum>
+      <section className="sub_vr ">
+        {state[0].Hotspot.map((img, i) => (
+          <div key={i}>
+            <img
+              width={200}
+              src={process.env.API_BASE_URL + img.vr_image?.Image?.url}
+              alt=""
+              onClick={(evt, name) => setstate(findImage(img.vr_image.id))}
+            />
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export function getStrapiURL(path = "") {
+  return `${process.env.API_BASE_URL}${path}`;
+}
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+// Helper to make GET requests to Strapi
+export async function fetchAPI(path) {
+  const requestUrl = getStrapiURL(path);
+  console.log("requsturl", requestUrl);
+  const response = await fetch(requestUrl);
+  const data = await response.json();
+  return data;
 }
